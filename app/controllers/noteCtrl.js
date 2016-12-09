69992695ffec09cee8ee8715dd8238ec03276b23
Abby Moore <abbyleighanne@gmail.com>
2016-12-08 19:12:46 -0600

    all notes show

--- app/controllers/noteCtrl.js
+++ app/controllers/noteCtrl.js
@@ -1,9 +1,10 @@
 "use strict";
 
-app.controller('ItemListCtrl', function($scope, ItemStorage){
+app.controller('ItemListCtrl', function($scope, $routeParams, ItemStorage){
 
 	ItemStorage.getItemList()
 		.then( (itemArray) => {
+			console.log(itemArray); 
 			$scope.items = itemArray;
 			$scope.$apply();
 	}); 
